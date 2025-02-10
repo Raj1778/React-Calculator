fdc34cf0ea24eff0a6aa5bc0ed955e641866b489
Raj Mehrotra <rajmhr078@gmail.com>
2025-02-10 21:37:57 +0530

    Initial commit

---
+++ src/components/ButtonsContainer.jsx
@@ -0,0 +1,39 @@
+import styles from "./ButtonsContainer.module.css";
+const ButtonsContainer = ({ onButtonClick }) => {
+  const buttonNames = [
+    "C",
+    "+/-",
+    "%",
+    "÷",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "0",
+    ".",
+    "=",
+  ];
+  return (
+    <div className={styles.buttonsContainer}>
+      {buttonNames.map((buttonName) => {
+        return (
+          <button
+            className={styles.button}
+            onClick={() => onButtonClick(buttonName)}
+          >
+            {buttonName}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+export default ButtonsContainer;
